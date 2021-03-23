2cbe42e9337b1dbe62dfbf6041433a74468acd68
LinPeiHsuan <bbaarrbbiiee23@gmail.com>
2021-03-23 15:39:26 +0800

    update our menu-items to navigate withRouter HigherOrderComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatPage = () => (
+    <div>
+      <h1>Hat Page</h1>
+    </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './directory.style.scss';
+import "./directory.style.scss";
 import MenuItem from "../menu-item/menu-item.component";
 
 // need to store the state values of the menu items that we want to pass and create the menu items
@@ -13,28 +13,33 @@ class Directory extends React.Component {
           title: "hat",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
+          linkUrl: "hats",
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
+          linkUrl: "jackets",
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
+          linkUrl: "sneakers",
         },
         {
           title: "womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: 'large',
+          size: "large",
           id: 4,
+          linkUrl: "womens",
         },
         {
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: 'large',
+          size: "large",
           id: 5,
+          linkUrl: "mens",
         },
       ],
     };
@@ -43,9 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-
+        {this.state.sections.map(({ id, ...otherSectionsProps }) => (
+          <MenuItem key={id} {...otherSectionsProps} />
         ))}
       </div>
     );
